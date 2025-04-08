c1c3f3a9b55567f80ea920a4a8885ccad5230089
sikong <1327325804@qq.com>
2025-04-08 21:53:37 +0800

    fix: 解决移动端微信播放不了问题

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -63,7 +63,7 @@ export default {
         // 获取el的id
         const video = el.querySelector("video")
         if (entry.isIntersecting) {
-          video.play()
+          // video.play()
           setTimeout(() => {
             video.play()
           },1000)
